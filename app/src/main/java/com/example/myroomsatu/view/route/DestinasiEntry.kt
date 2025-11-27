1e41c888d78e9ca6c1c2670dc0da9c2b03c5e0a5
hendr1an <hendriansyahgyan@gmail.com>
2025-11-27 14:31:46 +0700

    pemindahan direktori file destinasi

--- app/src/main/java/com/example/myroomsatu/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiEntry.kt
@@ -1,7 +1,5 @@
 package com.example.myroomsatu.view.route
 
-import com.example.myroomsatu.R // Kemungkinan import ini diperlukan jika menggunakan R.string
-
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Tambah Siswa"
--- app/src/main/java/com/example/myroomsatu/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiHome.kt
@@ -1,7 +1,5 @@
 package com.example.myroomsatu.view.route
 
-import com.example.myroomsatu.R
-
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = "Data Siswa"
