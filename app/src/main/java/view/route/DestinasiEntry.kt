3022f4f2363ffab13c64f9b03c7a55ad30362232
hendr1an <hendriansyahgyan@gmail.com>
2025-11-27 14:28:21 +0700

    menambahkan DestinasiEntry untuk navigasi halaman tambah siswa

---
+++ app/src/main/java/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.myroomsatu.view.route
+
+import com.example.myroomsatu.R // Kemungkinan import ini diperlukan jika menggunakan R.string
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Tambah Siswa"
+}
