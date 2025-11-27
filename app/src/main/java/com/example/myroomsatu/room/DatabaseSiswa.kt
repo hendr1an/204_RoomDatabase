4d07365c54872162853f1ff8b26ec50ee13c2aa9
hendr1an <hendriansyahgyan@gmail.com>
2025-11-27 09:42:04 +0700

    membuat DatabaseSiswa.kt

--- app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
@@ -1,8 +1,11 @@
 package com.example.myroomsatu.room
 
+import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
+import androidx.room.RoomDatabase
 
-class DatabaseSiswa
+interface DatabaseSiswa
 {
     @Database(entities = [Siswa::class], version = 1, exportSchema = false)
     abstract class DatabaseSiswa : RoomDatabase() {
