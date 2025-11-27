da77e514bd9ce1f48744516d0e79bc133cd4f02c
hendr1an <hendriansyahgyan@gmail.com>
2025-11-27 09:31:36 +0700

    membuat file DatabaseSiswa.kt

--- app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
@@ -1,4 +1,27 @@
 package com.example.myroomsatu.room
 
-class DatabaseSiswa {
+import androidx.room.Room
+
+class DatabaseSiswa
+{
+    @Database(entities = [Siswa::class], version = 1, exportSchema = false)
+    abstract class DatabaseSiswa : RoomDatabase() {
+
+        abstract fun siswaDao(): SiswaDao
+
+        companion object {
+            @Volatile
+            private var Instance: DatabaseSiswa? = null
+
+            fun getDatabase(context: Context): DatabaseSiswa {
+                return Instance ?: synchronized(this) {
+                    Room.databaseBuilder(
+                        context, DatabaseSiswa::class.java,
+                        "siswa_database")
+                        .build().also { Instance = it }
+                }
+            }
+        }
+    }
+
 }
