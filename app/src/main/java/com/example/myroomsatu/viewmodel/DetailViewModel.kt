0011657a15d23c7d0ccb29001060b119714e0691
hendr1an <hendriansyahgyan@gmail.com>
2025-12-04 14:31:52 +0700

    mengkonfigurasi ulang DetailViewModel.kt

--- app/src/main/java/com/example/myroomsatu/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/DetailViewModel.kt
@@ -4,10 +4,13 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myroomsatu.repositori.RepositoriSiswa
+import com.example.myroomsatu.view.route.DestinasiDetailSiswa
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-private val DetailViewModel.it: Any
 
 class DetailViewModel (
     savedStateHandle: SavedStateHandle,
