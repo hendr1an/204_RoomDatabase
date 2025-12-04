47f248c1a7849eb8da094e618e84c213af75e65d
hendr1an <hendriansyahgyan@gmail.com>
2025-12-04 13:34:49 +0700

    mengkonfigurasi ulang RepositoriSiswa.kt

--- app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
@@ -6,12 +6,16 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
+    fun getSiswaStream(id: Int): Flow<Siswa?> // Tambahan
     suspend fun insertSiswa(siswa: Siswa)
+    suspend fun deleteSiswa(siswa: Siswa)     // Tambahan
+    suspend fun updateSiswa(siswa: Siswa)     // Tambahan
 }
 
-class OfflineRepositoriSiswa(
-    private val siswaDao: SiswaDao
-): RepositoriSiswa {
+class OfflineRepositoriSiswa(private val siswaDao: SiswaDao) : RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id) // Tambahan
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)    // Tambahan
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)    // Tambahan
 }
